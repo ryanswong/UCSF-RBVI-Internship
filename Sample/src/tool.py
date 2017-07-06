41ab80fa5f09916361018623a01404553c7abc4d
suzuryu9000 <suzuryu9000@gmail.com>
2017-07-06 10:51:57 -0700

    enabled table sorter

--- Sample/src/tool.py
+++ Sample/src/tool.py
@@ -42,18 +42,15 @@ class ViewDockTool(ToolInstance):
         self._update_models()
 
     def _update_models(self, trigger=None, trigger_data=None):
+        import urllib.parse
+        from chimerax.core.commands.cli import StructuresArg
         # Called to update page with current list of models
         from chimerax.core.atomic import AtomicStructure
-<<<<<<< HEAD
-        html = ['<script type="text/javascript" src="/path/to/jquery-latest.js"></script>',
-                '<script type="text/javascript" src="/path/to/jquery.tablesorter.js"></script>',
-                '<h2><font color= "#FF3399">ViewDockX</font></h2>', "<ul>"]
-=======
-        html = ['<script type="text/javascript" src="/path/to/jquery-latest.js"></script>', 
-        '<script type="text/javascript" src="/path/to/jquery.tablesorter.js"></script>', '<h2 style="font-family:arial;"><font color= "#FF3399">ViewDockX</font></h2>', "<ul>"]
->>>>>>> 3f89dd6ec2c1437ab473917cf26fbb81b96854bf
         from urllib.parse import quote
-
+        html = ['<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>',
+                '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.28.15/js/jquery.tablesorter.js"></script>',
+                '<h2><font color= "#FF3399">ViewDockX</font></h2>',
+                "<ul>"]
 
         html.append("""<style>
                         table, th, td {
@@ -62,86 +59,71 @@ class ViewDockTool(ToolInstance):
                         }
                         </style>
                         
-                        <table style="width:100%">""")
-
-        # TRANSFERS ALL KEYS INTO A SET
+                        <table id="viewdockx_table" class="tablesorter" style="width:100%">""")
+                # TRANSFERS ALL KEYS INTO A SET
         category_set = set()
         for struct in self.structures:
             try:
                 category_set = {key for key in struct.viewdock_comment}
             except AttributeError:
                 pass
+        category_list = list(category_set)
 
         # ADDS ALL THE COLUMN HEADERS IN ALPHABETICAL ORDER
-<<<<<<< HEAD
-        # category_set = sorted(category_set)
-        html.append('<tr><th bgcolor= "#c266ff">ID</th>')
+        # category_list = sorted(category_list)
+        html.append('<thead><tr><th bgcolor= "#c266ff">ID</th>')
+
+
 
         #COLUMN HEADERS
-        html.append('<th bgcolor="#00FFCC">NAME</th>')
-        for category in category_set:
-=======
-        # s = sorted(s)
-        html.append('<tr><th style="font-family:arial;" bgcolor= "#c266ff">ID</th>')
+        html.append('<th bgcolor="#00FFCC">NAME</th>') #just for NAME header
 
-        #COLUMN HEADERS
-        html.append('<th style="font-family:arial;" bgcolor="#00FFCC">NAME</th>')
-        for category in s:
->>>>>>> 3f89dd6ec2c1437ab473917cf26fbb81b96854bf
+        for category in category_list:
             if category.upper() == "NAME":
                 pass
-            else:
-                html.append('<th style="font-family:arial;" bgcolor="#00FFCC">{}</th>'.format(category.upper()))
-        html.append("</tr>")
-
-
-
-
-
+            else: #all other headers
+                html.append('<th style="font-family:arial;" bgcolor="#00FFCC">{}</th>'\
+                    .format(category.upper()))
+        html.append("</tr></thead><tbody>")
         #COLUMN DATA
         for struct in self.structures:
             try:
                 comment_dict = struct.viewdock_comment
-            except AttributeError:
+            except AttributeError:  #for files with empty comment sections
                 comment_dict = {}
             html.append("<tr>")
-            html.append('<td  style="font-family:arial;" bgcolor="#ebccff" align="center"><a href=\"%s:%s\">%s - %s</a></td>' %
-                        (self.CUSTOM_SCHEME, quote(struct.atomspec()),  # "viewdock:#1.1"
-                         struct.id_string(), struct.name))
-            for category in category_set:
+            html.extend(['<td style="font-family:arial;" bgcolor="#ebccff" align="center">',
+                         '<input type="checkbox" />',
+                         '<a href=\"{}:{}\">{} - {}</a></td>'.format(self.CUSTOM_SCHEME,\
+                        quote(struct.atomspec()), struct.id_string(), struct.name)])
+
+            # ADDING VALUE FOR NAME
+            for category in category_list:
                 if category.upper() == "NAME":
                     try:
-<<<<<<< HEAD
                         html.append('<td bgcolor = "#CCFFF5" align="center">{}</td>'\
                             .format(comment_dict[category]))
                     except KeyError:
                         html.append('<td align="center">missing</td>')
-            for category in category_set:
-=======
-                        html.append('<td style="font-family:arial;" bgcolor = "#CCFFF5" align="center">{}</td>'.format(comment_dict[category]))
-                    except KeyError:
-                        html.append('<td style="font-family:arial;" align="center">missing</td>')
 
-            for category in s:
->>>>>>> 3f89dd6ec2c1437ab473917cf26fbb81b96854bf
+            #ADDING THE REST
+            for category in category_list:
                 try:
-                    if category.upper() == "NAME":
-                        pass
-                    else:
-<<<<<<< HEAD
+                    if category.upper() != "NAME":
                         html.append('<td bgcolor = "#CCFFF5" align="center">{}</td>'\
                             .format(comment_dict[category]))
-=======
-                        html.append('<td style="font-family:arial;" bgcolor = "#CCFFF5" align="center">{}</td>'.format(comment_dict[category]))
->>>>>>> 3f89dd6ec2c1437ab473917cf26fbb81b96854bf
                 except KeyError:
                     html.append('<td style="font-family:arial;" align="center">missing</td>')
             html.append("</tr>")
-
-
-        html.append("""</table>""")
-
-
+        html.append("</tbody></table>")
+        html.append("""<script>
+                $(document).ready(function() 
+                    { 
+                        $("#viewdockx_table").tablesorter(); 
+                    } 
+                );
+                </script>""")
+        print('\n'.join(html))
         self.html_view.setHtml('\n'.join(html))
 
     def _navigate(self, info):
@@ -158,7 +140,9 @@ class ViewDockTool(ToolInstance):
     def _run(self, atomspec):
         # Execute "sample count" command for given atomspec
         from chimerax.core.commands import run
+
         run(self.session, "select " + atomspec)
+        print("selected: ", atomspec)
         # from chimerax.core.logger import StringPlainTextLog
         # with StringPlainTextLog(self.session.logger) as log:
         #     try:
