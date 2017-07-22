f13ef218a1834cc4f4041d836a4ece808aedb8f1
suzuryu9000 <suzuryu9000@gmail.com>
2017-07-21 20:20:48 -0700

    moving files from Sample to ViewDockX folder. Removed Samples folder. Transfered static HTML, JS and CSS code to separate template file: viewdockx_frame.html. Created lib folder within src directory containing jquery and tablesorter js modules (However, not implemented within template as of yet)

---
+++ ViewDockX/build/lib/chimerax/viewdockx/io.py
@@ -0,0 +1,218 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+
+
+def open_mol2(session, stream, name):
+    structures = []
+    atoms = 0
+    bonds = 0
+    while True:
+        s = _read_block(session, stream)
+        if not s:
+            break
+        structures.append(s)
+        atoms += s.num_atoms
+        bonds += s.num_bonds
+    status = ("Opened mol2 file containing {} structures {} atoms, {} bonds".format
+              (len(structures), atoms, bonds))
+    return structures, status
+
+
+def _read_block(session, stream):
+    """function that calls subfunctions that each read a specific section of the mol2 file"""
+    # First section should be commented out
+    # Second section: "@<TRIPOS>MOLECULE"
+    # Third section: "@<TRIPOS>ATOM"
+    # Fourth section: "@<TRIPOS>BOND"
+    # Fifth section: "@<TRIPOS>SUBSTRUCTURE"
+
+    from numpy import (array, float64)
+    from chimerax.core.atomic import AtomicStructure
+
+    comment_dict, molecular_dict = read_com_and_mol(session, stream)
+    if not molecular_dict:
+        return None
+    atom_dict = read_atom(session, stream, int(molecular_dict["num_atoms"]))
+    bond_dict = read_bond(session, stream, int(molecular_dict["num_bonds"]))
+    substructure_dict = read_substructure(session, stream, int(molecular_dict["num_subst"])) #pass in # of substructures
+
+
+    s = AtomicStructure(session)
+
+    csd = build_residues(s, substructure_dict)
+    cad = build_atoms(s, csd, atom_dict)
+    build_bonds(s, cad, bond_dict)
+    s.viewdock_comment = comment_dict
+
+    return s
+
+
+def read_com_and_mol(session, stream):
+    """Parses commented section"""
+    import ast
+
+    comment_dict = {}
+
+
+    while True:
+        comment = stream.readline()
+        if not comment: 
+            break
+        if not comment_dict and comment[0] == "\n": #before the comment section
+            continue
+        if comment[0] != "#":  #for the end of comment section
+            break
+        line = comment.replace("#", "")
+        parts = line.split(":")
+        parts = [item.strip() for item in parts]
+        if ":" not in line:
+            for i in range(len(line), 1, -1):
+                if line[i-1] == " ":
+                    comment_dict[line[:i].strip()] = line[i:].strip()
+                    break
+        else:
+            comment_dict[(parts[0])] = parts[1]
+
+    if comment == "@<TRIPOS>MOLECULE":
+        pass
+    else:
+        molecule_line = stream.readline()
+
+        while "@<TRIPOS>MOLECULE" not in molecule_line:
+            if not molecule_line: #if not even "\n"
+                return None, None
+            molecule_line = stream.readline()
+
+    molecular_dict = {}
+    mol_labels = ["mol_name", ["num_atoms", "num_bonds", "num_subst", "num_feat", "num_sets"],\
+    "mol_type", "charge_type", "status_bits"]
+
+
+    for label in mol_labels:
+        molecule_line = stream.readline().split()
+        try:
+            # Only for the integers (num_atoms, num_bonds, etc)
+            if all(isinstance(int(item), int) for item in molecule_line):
+                molecular_dict.update(dict(zip(label, molecule_line)))
+        except (ValueError, SyntaxError):
+            molecular_dict[label] = molecule_line[0]
+
+    return comment_dict, molecular_dict
+
+
+def read_atom(session, stream, atom_count):
+    """parses atom section"""
+
+    import ast
+    while "@<TRIPOS>ATOM" not in stream.readline():
+        pass
+
+
+    atom_dict = {}
+
+    for _ in range(atom_count):
+        atom_line = stream.readline().strip()
+        if len(atom_line) == 0:
+            print("error: no line found")
+        parts = atom_line.split()
+        # parts wil be like, ['1', 'C1', 9.4819, 36.0139, 21.8847, 'C.3', 1, 'RIBOSE_MONOPHOSPHATE', 0.0767]
+        if len(parts) != 9:
+            print("error: not enough entries on line: ", atom_line)
+            return None
+
+        atom_dict[(parts[0])] = parts[1:]
+
+
+    # atom_dict would now be: 
+    # {1 : ['C1', '9.4819', '36.0139', '21.8847', 'C.3', '1', 'RIBOSE_MONOPHOSPHATE', '0.0767'],
+    # 2 : ['C2'....] }
+
+    return atom_dict
+
+def read_bond(session, stream, bond_count):
+    """parses bond section"""
+
+    while "@<TRIPOS>BOND" not in stream.readline():
+        pass
+
+    bond_dict = {}
+
+    for _ in range(bond_count):
+        bond_line = stream.readline()
+        parts = bond_line.split()
+        if len(parts) != 4:
+            print("error: not enough entries in under bond data")
+        if not isinstance(int(parts[0]), int):
+            print("error: first value is needs to be an integer")
+            return None
+
+        bond_dict[parts[0]] = parts[1:3]
+
+    return bond_dict
+
+def read_substructure(session, stream, num_subst):
+    """parses substructure section"""
+
+    while "@<TRIPOS>SUBSTRUCTURE" not in stream.readline():
+        pass
+
+    for _ in range(num_subst):
+        substructure_dict = {}
+        subst_line = stream.readline()
+        parts = subst_line.split()
+
+        substructure_dict[parts[0]] = parts[1:]
+
+    return substructure_dict
+
+def build_residues(s, substructure_dict):
+    """ create chimeraX substructure dictionary (csd) """
+    csd = {}
+    # csd will be something like {"1": <residue>}
+
+    for s_index in substructure_dict:
+
+
+        # new_residue(self, residue_name, chain_id, pos, insert=' ')
+
+        residue = s.new_residue(substructure_dict[s_index][0][:4], str(substructure_dict[s_index][5]), int(s_index))
+        csd[s_index] = residue
+    return csd
+
+
+def build_atoms(s, csd, atom_dict):
+
+    """ Creates chimeraX atom dictionary (cad)"""
+    from numpy import array, float64
+
+    cad = {}
+    for key in atom_dict:
+        name = atom_dict[key][0]
+        element = atom_dict[key][4]
+        if "." in element:
+            split_element = element.split(".")
+            element = split_element[0]
+        xyz = [float(n) for n in atom_dict[key][1:4]]
+        new_atom = s.new_atom(name, element)
+        new_atom.coord = array(xyz, dtype=float64)
+        # new_atom.serial_number = int(key)
+
+        # adding ne atom to subst_id 
+        csd[atom_dict[key][5]].add_atom(new_atom)
+
+        cad[key] = new_atom
+
+    return cad
+
+
+def build_bonds(s, cad, bond_dict):
+    for key in bond_dict:
+        atom1index = bond_dict[key][0]
+        atom2index = bond_dict[key][1]
+        try:
+            a1 = cad[atom1index]
+            a2 = cad[atom2index]
+        except KeyError:
+            print("Error : bad atom index in bond")
+        else:
+            s.new_bond(a1, a2)
---
+++ ViewDockX/src/tool.py
@@ -0,0 +1,241 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+from chimerax.core.atomic import AtomicStructure
+from chimerax.core.tools import ToolInstance
+
+
+class ViewDockTool(ToolInstance):
+
+    SESSION_ENDURING = False
+    SESSION_SKIP = True         # No session saving for now
+    CUSTOM_SCHEME = "viewdockx"    # HTML scheme for custom links
+    display_name = "ViewDockX"
+
+    def __init__(self, session, tool_name, structures=None):
+        # Standard template stuff for intializing tool
+        super().__init__(session, tool_name)
+        from chimerax.core.ui.gui import MainToolWindow
+        self.tool_window = MainToolWindow(self)
+        self.tool_window.manage(placement="side")
+        if structures is None:
+            structures = session.models.list(type=AtomicStructure)
+        self.structures = structures
+        parent = self.tool_window.ui_area
+
+        # Create an HTML viewer for our user interface.
+        # We can include other Qt widgets if we want to.
+        from PyQt5.QtWidgets import QGridLayout
+        from chimerax.core.ui.widgets import HtmlView
+        layout = QGridLayout()
+        self.html_view = HtmlView(parent, size_hint=(575, 200),
+                                  interceptor=self._navigate,
+                                  schemes=[self.CUSTOM_SCHEME])
+        layout.addWidget(self.html_view, 0, 0)  # row 0, column 0
+        parent.setLayout(layout)
+
+        # Register for model addition/removal so we can update model list
+        from chimerax.core.models import REMOVE_MODELS
+        t = session.triggers
+        # self._add_handler = t.add_handler(ADD_MODELS, self._update_models)
+        self._remove_handler = t.add_handler(
+            REMOVE_MODELS, self._update_models)
+
+        # Go!
+        self._update_models()
+
+    def delete(self):
+        t = self.session.triggers
+        if self._remove_handler:
+            t.remove_handler(self._remove_handler)
+            self._remove_handler = None
+        super().delete()
+
+    def _update_models(self, trigger=None, trigger_data=None):
+        # Called to update page with current list of models
+        from urllib.parse import urlunparse, urlparse, quote, parse_qs, parse_qsl, urlencode
+        # from chimerax.core.atomic import AtomicStructure
+
+        if trigger_data is not None:
+
+            for struct in self.structures:
+                if struct in trigger_data:
+                    self.structures.remove(struct)
+            if not self.structures:
+                self.delete()
+                return
+
+        # TRANSFERS ALL KEYS INTO A SET, THEN A LIST
+        category_set = set()
+        for struct in self.structures:
+            try:
+                category_set = {key for key in struct.viewdock_comment}
+            except AttributeError:
+                pass
+        category_list = sorted(list(category_set))
+
+        ####################
+        ####    TABLE   ####
+        ####################
+
+        table = (['<table id="viewdockx_table" \
+            class="tablesorter" style="width:100%">'])
+
+        ###########################
+        ###    COLUMN HEADERS   ###
+        ###########################
+
+        #   COLUMN HEADER    | ID |
+        table.append('<thead><tr>')
+        table.append('<th class="id">ID</th>')
+
+        #   COLUMN HEADERS    | NAME |...|...|...
+        table.append('<th>NAME</th>')
+        for category in category_list:
+            if category.upper() == "NAME":
+                pass
+            else:
+                table.append('<th>{}</th>'.format(category.upper()))
+        table.append("</tr></thead>")
+
+        ########################
+        ###    COLUMN DATA   ###
+        ########################
+        table.append('<tbody>')
+        for struct in self.structures:
+            try:
+                comment_dict = struct.viewdock_comment
+            except AttributeError:  # for files with empty comment sections
+                comment_dict = {}
+
+            # MAKES THE URL FOR EACH STRUCTURE
+            args = [("atomspec", struct.atomspec())]
+            query = urlencode(args)
+            url = urlunparse((self.CUSTOM_SCHEME, "", "", "", query, ""))
+
+            # ADDING ID VALUE
+            table.append("<tr>")
+            table.extend(['<td class="id">',
+                          # for checkbox + atomspec string
+                          '<span style="display:none; white-space: nowrap" class="checkbox">\
+                         <input class="checkbox, struct" type="checkbox" href="{}"/>\
+                         {}</span>'.format(url, struct.atomspec()[1:]),
+
+                          # for atomspec links only
+                          '<span class="link"><a href="{}">{}</a></span>'\
+                          .format(url, struct.atomspec()[1:]),
+                          '</td>'])
+
+            # ADDING VALUE FOR NAME
+            for category in category_list:
+                if category.upper() == "NAME":
+                    try:
+                        table.append(
+                            '<td>{}</td>'.format(comment_dict[category]))
+                    except KeyError:
+                        table.append('<td>missing</td>')
+
+            # ADDING THE REST
+            for category in category_list:
+                try:
+                    if category.upper() != "NAME":
+                        table.append('<td>{}</td>'
+                                     .format(comment_dict[category]))
+                except KeyError:
+                    table.append('<td>missing</td>')
+            table.append("</tr>")
+        table.append("</tbody>")
+        table.append("</table>")
+
+        import os
+
+        # os.path.join()
+
+        dir_path = os.path.dirname(os.path.abspath(__file__))
+        lib_path = os.path.join(dir_path, "lib")
+
+        with open(os.path.join(dir_path, "viewdockx_frame.html"), "r") as file:
+            template = file.read()
+        output = template.replace("TABLE", ('\n'.join(table)))\
+                         .replace("jquery_path", lib_path)\
+                         .replace("tablesorter_path", lib_path)
+
+        self.html_view.setHtml(output)
+        # print('\n'.join(template))
+        print(output)
+
+    def _navigate(self, info):
+        # Called when link is clicked.
+        # "info" is an instance of QWebEngineUrlRequestInfo
+        from chimerax.core.commands.cli import StructuresArg
+        from urllib.parse import parse_qs
+        url = info.requestUrl()
+        scheme = url.scheme()
+        if scheme == self.CUSTOM_SCHEME:
+            # Intercept our custom scheme.
+            # Method may be invoked in a different thread than
+            # the main thread where Qt calls may be made.
+
+            query = parse_qs(url.query())
+            if "show_all" in query.keys():
+                show_all = query["show_all"][0]
+                self.session.ui.thread_safe(self._checkall, show_all)
+            else:
+                try:
+                    atomspec = query["atomspec"][0]
+                    disp = query["display"][0]
+                except (KeyError, ValueError):
+                    atomspec = "missing"
+                # print("atomspec:", atomspec)
+                # print("checkpoint 3")
+                structures, text, remainder = StructuresArg.parse(
+                    atomspec, self.session)
+                self.session.ui.thread_safe(self._run, structures, disp)
+
+    def _run(self, structures, disp):
+        # ###Execute "sample count" command for given atomspec
+        # from chimerax.core.commands import run
+
+        # for struct in self.structures:
+        #     struct.display = struct in structures
+
+        if disp == "0":
+            for struct in self.structures:
+                if structures[0] == struct:
+                    struct.display = False
+        # elif disp == "2":
+        #     for struct in self.structures:
+        #         struct.display = struct in structures
+        else:
+            for struct in self.structures:
+                if structures[0] == struct:
+                    struct.display = True
+
+    def _checkall(self, show_all):
+
+        if show_all == "true":
+            for struct in self.structures:
+                struct.display = True
+
+        else:
+            for struct in self.structures:
+                struct.display = False
+
+        # run(self.session, "select " + text)
+        # from chimerax.core.logger import StringPlainTextLog
+        # with StringPlainTextLog(self.session.logger) as log:
+        #     try:
+        #     finally:
+        #         html = "<pre>\n%s</pre>" % log.getvalue()
+        #         js = ('document.getElementById("output").innerHTML = %s'
+        #               % repr(html))
+        #         self.html_view.page().runJavaScript(js)
+
+
+# ## TEST PURPOSE ONLY ####
+
+# def test_run(file_name):
+#     import os
+#     file = os.path.join(os.getcwd(), 'example_files/{}'.format(file_name))
+#     with open(file, "r") as stream:
+#         open_mol2(None, stream, file)
+
+# test_run("ras.mol2")
