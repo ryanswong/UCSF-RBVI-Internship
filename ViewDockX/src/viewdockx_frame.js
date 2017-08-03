7bbda8a00ad345c7a198d1a45166063ffe7fed02
hku8 <hannahku@gmail.com>
2017-08-03 10:04:43 -0700

    add d3 histogram template
    add d3 histogram template

--- ViewDockX/src/viewdockx_frame.js
+++ ViewDockX/src/viewdockx_frame.js
@@ -81,46 +81,50 @@ function init() {
     });
 
     $('#histogram_btn').on('click', function() {
-        if (typeof vdx_chart != 'undefined') {
-            vdx_chart.destroy();
-        }
-        var ctx = document.getElementById("viewdock_histogram").getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-               data: {
-                labels: data_array, //bar labels (score) ex. 1-10
-                datasets: [{
-                    label: property,
-                    data: [123, 123, 2, 123, 23, 23], // count
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
-                }
-            }
-        });
+        var data = d3.range(1000).map(d3.randomBates(10));
+
+        var formatCount = d3.format(",.0f");
+
+        var svg = d3.select("svg"),
+            margin = {top: 10, right: 30, bottom: 30, left: 30},
+            width = +svg.attr("width") - margin.left - margin.right,
+            height = +svg.attr("height") - margin.top - margin.bottom,
+            g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.scaleLinear()
+            .rangeRound([0, width]);
+
+        var bins = d3.histogram()
+            .domain(x.domain())
+            .thresholds(x.ticks(20))
+            (data);
+
+        var y = d3.scaleLinear()
+            .domain([0, d3.max(bins, function(d) { return d.length; })])
+            .range([height, 0]);
+
+        var bar = g.selectAll(".bar")
+          .data(bins)
+          .enter().append("g")
+            .attr("class", "bar")
+            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+
+        bar.append("rect")
+            .attr("x", 1)
+            .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+            .attr("height", function(d) { return height - y(d.length); });
+
+        bar.append("text")
+            .attr("dy", ".75em")
+            .attr("y", 6)
+            .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+            .attr("text-anchor", "middle")
+            .text(function(d) { return formatCount(d.length); });
+
+        g.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
     });
 
 
