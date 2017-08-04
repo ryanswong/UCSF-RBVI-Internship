5b02e6d94440c342384f00f23e315b0ba597b1df
suzuryu9000 <suzuryu9000@gmail.com>
2017-08-03 23:38:26 -0700

    made copy of io.py to fix compatibility with new mol2 file

--- ViewDockX/src/io - Copy.py
+++ ViewDockX/src/io - Copy.py
@@ -1,21 +1,26 @@
-# vim: set expandtab shiftwidth=4 softtabstop=4:
-
-
-
 def open_mol2(session, stream, name):
     structures = []
     atoms = 0
     bonds = 0
     while True:
         s = _read_block(session, stream)
+        # print(s)
         if not s:
             break
-        structures.append(s)
-        atoms += s.num_atoms
-        bonds += s.num_bonds
-    status = ("Opened mol2 file containing {} structures {} atoms, {} bonds".format
-              (len(structures), atoms, bonds))
-    return structures, status
+        # structures.append(s)
+        # atoms += s.num_atoms
+        # bonds += s.num_bonds
+    # status = ("Opened mol2 file containing {} structures ({} atoms, {} bonds)".format
+    #           (len(structures), atoms, bonds))
+    # return structures, status
+
+
+
+def print_dict(dict):
+    # print(s)
+    for key, value in dict.items():
+        print(key, ":", value,)
+    print()
 
 
 def _read_block(session, stream):
@@ -29,22 +34,35 @@ def _read_block(session, stream):
     from numpy import (array, float64)
     from chimerax.core.atomic import AtomicStructure
 
-    comment_dict, molecular_dict = read_com_and_mol(session, stream)
+    # print("TEST1")
+
+    comment_dict , molecular_dict= read_com_and_mol(session, stream)
     if not molecular_dict:
         return None
+    print_dict(comment_dict)
+    print_dict(molecular_dict)
+
+
     atom_dict = read_atom(session, stream, int(molecular_dict["num_atoms"]))
+    print_dict(atom_dict)
+
+
     bond_dict = read_bond(session, stream, int(molecular_dict["num_bonds"]))
-    substructure_dict = read_substructure(session, stream, int(molecular_dict["num_subst"])) #pass in # of substructures
+    print_dict(bond_dict)
 
 
-    s = AtomicStructure(session)
+    # substructure_dict = read_substructure(session, stream, int(molecular_dict["num_subst"])) #pass in # of substructures
+    # print_dict(substructure_dict)
 
-    csd = build_residues(s, substructure_dict)
-    cad = build_atoms(s, csd, atom_dict)
-    build_bonds(s, cad, bond_dict)
-    s.viewdock_comment = comment_dict
 
-    return s
+    print()
+    print("########")
+    print("success")
+    print("########")
+    print()
+
+
+    return True
 
 
 def read_com_and_mol(session, stream):
@@ -54,8 +72,11 @@ def read_com_and_mol(session, stream):
     comment_dict = {}
 
 
+    # Comment section
+
     while True:
         comment = stream.readline()
+        # print(comment)
         if not comment: 
             break
         if not comment_dict and comment[0] == "\n": #before the comment section
@@ -73,31 +94,53 @@ def read_com_and_mol(session, stream):
         else:
             comment_dict[(parts[0])] = parts[1]
 
+    # print("CHECKPOINT1")
+
+    # Molecule section
+
     if comment == "@<TRIPOS>MOLECULE":
         pass
     else:
         molecule_line = stream.readline()
 
         while "@<TRIPOS>MOLECULE" not in molecule_line:
-            if not molecule_line: #if not even "\n"
+            if not molecule_line: #if it's not even "\n".  is true if at the end of a file
                 return None, None
             molecule_line = stream.readline()
+
+    # return comment_dict, None
 
     molecular_dict = {}
     mol_labels = ["mol_name", ["num_atoms", "num_bonds", "num_subst", "num_feat", "num_sets"],\
     "mol_type", "charge_type", "status_bits"]
 
+    line_num = 0
+
 
     for label in mol_labels:
-        molecule_line = stream.readline().split()
-        try:
-            # Only for the integers (num_atoms, num_bonds, etc)
+        line_num += 1
+        # print("line_num: ", line_num)
+        if line_num == 1:
+            molecule_line = stream.readline().strip()
+            while not molecule_line:
+                molecule_line = stream.readline().strip()
+            molecular_dict[label] = molecule_line
+            continue
+        
+
+        # Only for the integers (num_atoms, num_bonds, etc)
+        if line_num == 2:
+            molecule_line = stream.readline().split()
             if all(isinstance(int(item), int) for item in molecule_line):
                 molecular_dict.update(dict(zip(label, molecule_line)))
-        except (ValueError, SyntaxError):
-            molecular_dict[label] = molecule_line[0]
-
-
+            else:
+                raise ValueError("Second line needs to be series of integers")
+            
+        else:
+            molecule_line = stream.readline().strip()
+            molecular_dict[label] = molecule_line
+        if "@<TRIPOS>ATOM" in molecule_line:
+            break
 
     return comment_dict, molecular_dict
 
@@ -118,7 +161,7 @@ def read_atom(session, stream, atom_count):
             print("error: no line found")
         parts = atom_line.split()
         # parts wil be like, ['1', 'C1', 9.4819, 36.0139, 21.8847, 'C.3', 1, 'RIBOSE_MONOPHOSPHATE', 0.0767]
-        if len(parts) not in range(6, 11): # must be within 6-10 entries
+        if len(parts) != 9:
             print("error: not enough entries on line: ", atom_line)
             return None
 
@@ -167,54 +210,36 @@ def read_substructure(session, stream, num_subst):
 
     return substructure_dict
 
-def build_residues(s, substructure_dict):
-    """ create chimeraX substructure dictionary (csd) """
-    csd = {}
-    # csd will be something like {"1": <residue>}
 
-    for s_index in substructure_dict:
-
-
-        # new_residue(self, residue_name, chain_id, pos, insert=' ')
-
-        residue = s.new_residue(substructure_dict[s_index][0][:4], str(substructure_dict[s_index][5]), int(s_index))
-        csd[s_index] = residue
-    return csd
+# ## TEST PURPOSE ONLY ####
+def test_run(file_name):
+    import os
+    file = os.path.join(os.getcwd(), 'example_files/{}'.format(file_name))
+    # print(open(file, "r").read())
+    with open(file, "r") as stream:
+        open_mol2(None, stream, file)
 
 
-def build_atoms(s, csd, atom_dict):
-
-    """ Creates chimeraX atom dictionary (cad)"""
-    from numpy import array, float64
-
-    cad = {}
-    for key in atom_dict:
-        name = atom_dict[key][0]
-        element = atom_dict[key][4]
-        if "." in element:
-            split_element = element.split(".")
-            element = split_element[0]
-        xyz = [float(n) for n in atom_dict[key][1:4]]
-        new_atom = s.new_atom(name, element)
-        new_atom.coord = array(xyz, dtype=float64)
-        # new_atom.serial_number = int(key)
-
-        # adding ne atom to subst_id 
-        csd[atom_dict[key][5]].add_atom(new_atom)
-
-        cad[key] = new_atom
-
-    return cad
+test_run("poses(short).mol2")
+# test_run("ras(short).mol2")
 
 
-def build_bonds(s, cad, bond_dict):
-    for key in bond_dict:
-        atom1index = bond_dict[key][0]
-        atom2index = bond_dict[key][1]
-        try:
-            a1 = cad[atom1index]
-            a2 = cad[atom2index]
-        except KeyError:
-            print("Error : bad atom index in bond")
-        else:
-            s.new_bond(a1, a2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
